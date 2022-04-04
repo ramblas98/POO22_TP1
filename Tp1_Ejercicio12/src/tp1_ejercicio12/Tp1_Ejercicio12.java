8c636607e8b55b382358cd82bf40b54768dc9d64
Matias <Matias@LAPTOP-PVUVC0VL>
2022-04-04 15:59:31 -0300

    Hola

--- Tp1_Ejercicio12/src/tp1_ejercicio12/Tp1_Ejercicio12.java
+++ Tp1_Ejercicio12/src/tp1_ejercicio12/Tp1_Ejercicio12.java
@@ -16,6 +16,7 @@ public class Tp1_Ejercicio12 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola");
     }
     
 }
