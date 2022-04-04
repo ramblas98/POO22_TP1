96ceb8a4263e4f6cfcd7b0da5533d212238701df
ramblas98 <blaslopez@gmail.com>
2022-04-04 15:28:50 -0300

    Agregue hola

--- POO22_TP1_EJ12/src/poo22_tp1_ej12/POO22_TP1_EJ12.java
+++ POO22_TP1_EJ12/src/poo22_tp1_ej12/POO22_TP1_EJ12.java
@@ -15,6 +15,7 @@ public class POO22_TP1_EJ12 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola github.");
     }
     
 }
